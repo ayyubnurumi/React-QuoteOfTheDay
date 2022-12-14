dca58280543a48246e512e9f2e85b676c19f14b3
ayyubnurumi <ayyubinvestment@gmail.com>
2022-12-14 18:04:38 +0700

    qotd-app first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useState, useEffect, useCallback } from "react";
+import "./App.css";
 
 function App() {
+  const [respData, setRespData] = useState("");
+  
+  const fetchData = useCallback(
+    () => {
+      axios({
+        method: "GET",
+        url: "https://quotes15.p.rapidapi.com/quotes/random/",
+        headers: {
+          "content-type": "application/octet-stream",
+          "X-RapidAPI-Host": "quotes15.p.rapidapi.com",
+          "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
+        },
+        params: { language_code: "en" },
+      })
+        .then((response) => setRespData(response.data))
+        .catch((error) => console.log(error));
+    },
+    [],
+  )
+  
+  useEffect(() => {fetchData()
+    // setRespData('hello')
+    // console.log(respData);
+  }, [fetchData]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Quote of the Day</h1>
       </header>
+      <main>
+        {respData && (
+          <blockquote>
+            "{respData && respData.content}"
+            <small>
+              {respData && respData.originator && respData.originator.name}
+            </small>
+          </blockquote>
+        )}
+      </main>
+      <button onClick={fetchData}>Get Quote</button>
+      {/* <pre>
+        <code>{respData && JSON.stringify(respData, null, 4)}</code>
+      </pre> */}
     </div>
   );
 }
